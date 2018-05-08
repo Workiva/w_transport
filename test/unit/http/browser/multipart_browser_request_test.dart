bf74d8bb4bed2a83a62dc63d6bd32dd4754eef21
Todd Beckman <todd.beckman@workiva.com>
2018-05-08 11:04:46 -0600

    AF-157 dont report repeated data

---
+++ test/unit/http/browser/multipart_browser_request_test.dart
@@ -0,0 +1,51 @@
+// Copyright 2018 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+@TestOn('browser')
+import 'dart:convert';
+
+import 'package:test/test.dart';
+import 'package:w_transport/browser.dart' show browserTransportPlatform;
+import 'package:w_transport/src/http/browser/form_data_body.dart';
+import 'package:w_transport/src/http/browser/multipart_request.dart';
+import 'package:w_transport/w_transport.dart' as transport;
+
+import '../../../naming.dart';
+
+void main() {
+  final naming = new Naming()
+    ..testType = testTypeUnit
+    ..topic = topicHttp;
+
+  group(naming.toString(), () {
+    group('BrowserMultipartRequest', () {
+      group('finalizeBody', () {
+        test('does not include duplicate fields', () async {
+          final key = 'ascii';
+          final value = ASCII.decode(ASCII.encode("This is ASCII!"));
+
+          final BrowserMultipartRequest request =
+              new transport.MultipartRequest(
+                  transportPlatform: browserTransportPlatform)
+                ..fields = {
+                  'ascii': ASCII.decode(ASCII.encode("This is ASCII!")),
+                };
+
+          final FormDataBody body = await request.finalizeBody();
+          expect(body.formData.getAll('ascii'), equals([value]));
+        });
+      });
+    });
+  });
+}

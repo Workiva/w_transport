51b124676a7e37abcff9d0771148d8605d1fadaf
wesleybalvanz-wf <wesley.balvanz@workiva.com>
2022-06-01 13:09:13 -0500

    Only wait 15 seconds between tries and remove some debugging code

--- skynet.py
+++ skynet.py
@@ -6,14 +6,7 @@ import time
 commit_hash = os.getenv("SKYNET_BUILD_COMMIT_HASH")
 branch_name = os.getenv("SKYNET_CHECKOUT_REF").split("/")[-1]
 github_token = os.getenv("GITHUB_TOKEN")
-res = requests.get(
-        "https://api.github.com/users/octocat",
-        params={"branch": branch_name},
-        headers={
-            'Accept': 'application/vnd.github.v3+json',
-            'Authorization': f'token {github_token}'
-        })
-print(res.json())
+
 completed = False
 while not completed:
     try:
@@ -38,13 +31,13 @@ while not completed:
             print(f"Head sha: {head_sha} does not match expected commit hash: {commit_hash} continuing")
             continue
         if run.get("status") != "completed":
-            print("Run not completed yet waiting a minute and trying again.")
-            time.sleep(60)
+            print("Run not completed yet waiting 15 seconds and retrying.")
+            time.sleep(15)
             break
         conclusion = run.get("conclusion")
         if conclusion == "success":
             print("Github actions test run successful")
-            exit(0)
+            completed = True
         else:
             print(f"Github actions failed with the conclusion of: {conclusion}")
             exit(1)
